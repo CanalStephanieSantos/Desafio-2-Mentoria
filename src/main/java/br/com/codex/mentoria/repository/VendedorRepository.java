37f322df520a907c4727693e9a1da6b301f97781
Gabriel Gama <58527214+gamagabriel@users.noreply.github.com>
2022-05-31 15:34:38 -0300

    Criação endpoint Get ID vendedor

--- src/main/java/br/com/codex/mentoria/repository/VendedorRepository.java
+++ src/main/java/br/com/codex/mentoria/repository/VendedorRepository.java
@@ -2,8 +2,12 @@ package br.com.codex.mentoria.repository;
 
 import br.com.codex.mentoria.model.Vendedor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
+    List<Vendedor> findVendedorById(@Param("id") Long id);
 }
